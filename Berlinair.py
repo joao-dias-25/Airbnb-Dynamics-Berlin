47c12c9b1f69fffbe1cec5ea9e478f0152ec7feb
joao-dias-25 <joaopbdias@gmail.com>
2020-12-07 15:41:31 +0100

    qrcode

--- Berlinair.py
+++ Berlinair.py
@@ -15,7 +15,7 @@ page = PAGES[selection]
 
 
 from PIL import Image
-st.sidebar.image(Image.open('uca-url.png'), caption='Donation Address: bitcoin', width= 250)
+st.sidebar.image('uca-url.png', caption='Donation Address: bitcoin', width= 250)
 #st.sidebar.image(Image.open('ethcode.jpeg'), caption='Donation Address: ethereum', width= 250)
 page.app()
 
